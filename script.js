946df9e7aa45d15411e0d9d7055c29bb23dcac43
callseven <callseven@me.com>
2021-06-01 21:33:04 -0300

    Projeto Finalizado

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 let input = document.getElementById("item");
 let button = document.getElementById("button");
 let ul = document.getElementById("itens");
+let itens = document.getElementsByTagName("li");
+let itemButtons = document.getElementsByClassName("remove");
 
-function criarElemento(){
+function  criarElemento(){
     let li = document.createElement("li");
 
     let xButton = document.createElement("button");
     xButton.innerHTML = "X";
+    xButton.className = "remove";
     li.appendChild(xButton);
 
     let text = document.createTextNode(input.value);
-
+    li.appendChild(text);
     ul.appendChild(li);
+    input.value = "";
+    buttonEvents();
+}
 
+function deleteItem (){
+    this.parentElement.remove();
+}
+
+function buttonEvents (){
+    for (let i =0; i < itemButtons.length ; i++ ){
+        itemButtons[i].addEventListener("click", deleteItem);
+    }
 }
 
 button.addEventListener("click", criarElemento);
+buttonEvents();
